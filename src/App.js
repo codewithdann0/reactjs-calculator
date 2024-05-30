414bd63143afeb2a08369dc2378777e1abbe6877
codewithdann0 <codewithdann028@gmail.com>
2024-05-30 09:39:46 +0300

    Added production build for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
+
+
+
+const App = () => {
+  const [input, setInput] = useState("");
+
+  const handleClick = (value) => {
+    setInput((prev) => prev + value);
+  };
+
+  const handleClear = () => {
+    setInput("");
+  };
+
+  const handleEqual = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput("Error");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <i class="fa-brands fa-react" id="logo"></i>
+      <div id="container">
+        <textarea value={input} id="display" readOnly></textarea>
+
+        <div>
+          <button id="clear" onClick={handleClear}>
+            AC
+          </button>
+          <button id="divide" onClick={() => handleClick("/")}>
+            /
+          </button>
+          <button id="multiply" onClick={() => handleClick("*")}>
+            *
+          </button>
+        </div>
+        <div>
+          <button id="seven" onClick={() => handleClick("7")}>
+            7
+          </button>
+          <button id="eight" onClick={() => handleClick("8")}>
+            8
+          </button>
+          <button id="nine" onClick={() => handleClick("9")}>
+            9
+          </button>
+          <button id="subtract" onClick={() => handleClick("-")}>
+            -
+          </button>
+        </div>
+        <div>
+          <button id="four" onClick={() => handleClick("4")}>
+            4
+          </button>
+          <button id="five" onClick={() => handleClick("5")}>
+            5
+          </button>
+          <button id="six" onClick={() => handleClick("6")}>
+            6
+          </button>
+          <button id="add" onClick={() => handleClick("+")}>
+            +
+          </button>
+        </div>
+        <div>
+          <button id="one" onClick={() => handleClick("1")}>
+            1
+          </button>
+          <button id="two" onClick={() => handleClick("2")}>
+            2
+          </button>
+          <button id="three" onClick={() => handleClick("3")}>
+            3
+          </button>
+          <button id="percent" onClick={() => handleClick("%")}>
+            %
+          </button>
+        </div>
+        <div>
+          <button id="zero" onClick={() => handleClick("0")}>
+            0
+          </button>
+          <button id="decimal" onClick={() => handleClick(".")}>
+            .
+          </button>
+          <button id="equal" onClick={handleEqual}>
+            =
+          </button>
+        </div>
+      </div>
+      <div id="info">
+        <h3>Design and coded</h3>
+        <p>By Daniel Tigistu</p>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
